773381ce1c6dc13e597bda10d15c065db1566ecc
giuliagiani2 <giani.giulia2004@gmail.com>
2024-08-29 10:54:01 +0100

    aggiunte alcune proprietà e metodi all'es precedente

--- modulo5/index2.js
+++ modulo5/index2.js
@@ -2,17 +2,34 @@
 class Automobile {
     marca = "";
     modello = "";
-    anno = ;
-    constructor(marca, modello, anno) {
+    anno = 0;
+
+    //AGGIUNGI METODI & PROPRIETA'
+    chilometraggio = 0;
+
+    constructor(marca, modello, anno, chilometraggio) {
         this.marca = marca;
         this.modello = modello;
         this.anno = anno;
+        this.chilometraggio = chilometraggio;
     }
     destrizione() {
         return `Automobile: ${this.marca} ${this.modello} ${this.anno}`;
+    }
+
+    //AGGIUNGI METODI & PROPRIETA'
+    aggiungiChilometri(km) {
+        if (km > 0) {
+            this.chilometraggio += km;
+        } else {
+            console.log("Inserire valore positivo");
+        }
     }
 }
 
 //AUTOMOBILE
 const miaAuto = new Automobile("Opel", "Corsa", 2022);
 console.log(miaAuto.destrizione);
+
+//AGGIUNGI METODI & PROPRIETA'
+miaAuto.aggiungiChilometri(10);
