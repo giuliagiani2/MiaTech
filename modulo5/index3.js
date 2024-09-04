6bec68f492838e277da0d6648489be212337f10e
giuliagiani2 <giani.giulia2004@gmail.com>
2024-09-04 11:24:22 +0100

    creato array con meno di 5 elementi e destrutturato

--- modulo5/index3.js
+++ modulo5/index3.js
@@ -4,3 +4,13 @@ const array = [1, 2, 3, 4, 5];
 let [valore1, , valore3, altriValori] = array;
 console.log("Valore 1: " + valore1);
 console.log("Valore 3: " + valore3);
+
+
+//DESTRUCTURING CON VALORI PREDEFINITI
+const array2 = ["a", "b", "c", "d"];
+let [primo, secondo, terzo, quarto, altro] = array2;
+console.log("prima lettera: " + primo);
+console.log("seconda lettera: " + secondo);
+console.log("terza lettera: " + terzo);
+console.log("quarta lettera: " + quarto);
+console.log("altre lettere: " + altro);
