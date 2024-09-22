5bff1056bc3da678a9a0ee5b21f7157641f8f9de
giuliagiani2 <giani.giulia2004@gmail.com>
2024-09-22 20:41:25 +0100

    correzione es copia di un array

--- modulo5/index3.js
+++ modulo5/index3.js
@@ -51,7 +51,7 @@ console.log("Paesaggio: " + paesaggio);
 
 //COPIA UN ARRAY
 let numbers = [11, 12, 13, 14, 15];
-const numbersSpread = [...numbers];
+let numbersSpread = [...numbers];
 
 console.log("Array originale" + numbers);
 console.log("Array copia" + numbersSpread);
