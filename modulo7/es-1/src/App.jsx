2f8824a5179f57392d97dacf0fc6babe81ac33b2
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-02 11:37:25 +0100

    seguiti i primi 4 esercizi

---
+++ modulo7/es-1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Helloworld from "./Helloworld";
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld />
+    </div>
+  )
+}
+
+export default App;
---
+++ modulo7/es-1/src/Helloworld.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Helloworld extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <h1>Hello world</h1>
+                    <p>First program in react</p>
+                </div>
+            </>
+        );
+    };
+}
+
+export default Helloworld;
