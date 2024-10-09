33647943a898224d856e7ac3e5a122a1b99e0e60
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-09 10:04:03 +0100

    inserito useState

--- modulo7/es-2/src/Counter.jsx
+++ modulo7/es-2/src/Counter.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 //CREATO UN CONTATORE SEMPLICE
 const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
     return (
         <div>
             <p>Counter: 0</p>
