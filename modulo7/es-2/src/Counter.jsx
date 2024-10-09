83a8efced9edc69c4d9f793c1915041dde6d8b22
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-09 10:19:08 +0100

    aggiunto bottone decrementante

--- modulo7/es-2/src/Counter.jsx
+++ modulo7/es-2/src/Counter.jsx
@@ -4,16 +4,26 @@ import { useState } from "react";
 //CREATO UN CONTATORE SEMPLICE
 const Counter = () => {
     const [counter, setCounter] = useState(0);
+
+    //INCREMENTA CONTATORE
     const handleClickIncrement = () => {
         setCounter((_counter) => {
             return _counter + 1;
         })
     }
 
+    //DECREMENTA CONTATORE
+    const handleClickDecrement = () => {
+        setCounter((counter) => {
+            return counter - 1;
+        })
+    }
+
     return (
         <div>
             <p>Counter: {counter}</p>
-            <button onClick={handleClickIncrement}>Invia</button>
+            <button onClick={handleClickIncrement}>Incrementa</button>
+            <button onClick={handleClickDecrement}>Decrementa</button>
         </div>
     )
 }
