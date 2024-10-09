be9e0a60e05e482a15a06adb257267d8f526e903
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-09 10:06:55 +0100

    aggiunto bottone incrementante

--- modulo7/es-2/src/Counter.jsx
+++ modulo7/es-2/src/Counter.jsx
@@ -4,10 +4,16 @@ import { useState } from "react";
 //CREATO UN CONTATORE SEMPLICE
 const Counter = () => {
     const [counter, setCounter] = useState(0);
+    const handleClickIncrement = () => {
+        setCounter((_counter) => {
+            return _counter + 1;
+        })
+    }
 
     return (
         <div>
-            <p>Counter: 0</p>
+            <p>Counter: {counter}</p>
+            <button onClick={handleClickIncrement}>Invia</button>
         </div>
     )
 }
