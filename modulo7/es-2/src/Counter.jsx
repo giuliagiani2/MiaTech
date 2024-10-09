c280404342d2dd20c8af839e537b7d099f1ff0f9
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-09 10:01:45 +0100

    creato componente Counter

---
+++ modulo7/es-2/src/Counter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//CREATO UN CONTATORE SEMPLICE
+const Counter = () => {
+    return (
+        <div>
+            <p>Counter: 0</p>
+        </div>
+    )
+}
+
+export default Counter;
