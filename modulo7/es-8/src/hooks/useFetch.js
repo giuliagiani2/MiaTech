e516deb7ca5a44e0c2e80c35c9c1c5ec620d07e6
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-16 21:23:02 +0100

    creata hook useFetch

---
+++ modulo7/es-8/src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+//IMPLEMENTA L'HOOK USEFETCH
+import { useEffect, useState } from "react";
+
+const DEFAULT_OPTIONS = {
+    method: "GET",
+    headers: {},
+    data: {}
+};
+
+export const usefetch = (url, options = DEFAULT_OPTIONS) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async () => {
+        if (!loading) setLoading(true);
+        if (error) setError(false);
+
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error("Errore durante la chiamata");
+            }
+            const _data = await response.json();
+            setData(_data);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            if (loading) setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [url]);
+
+    return {
+        data, error, loading
+    };
+}
