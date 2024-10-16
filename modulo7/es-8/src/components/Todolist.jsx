2e0ed0f002c7756892711ca8ead3a5f619356da3
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-16 22:32:18 +0100

    usato useCallback per ottimizzare la funzione di ricerca

--- modulo7/es-8/src/components/Todolist.jsx
+++ modulo7/es-8/src/components/Todolist.jsx
@@ -1,6 +1,6 @@
 //UTILIZZA USEFETCH IN UN COMPONENTE
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { useFilteredTodos } from "../hooks/useFilteredTodos";
 
@@ -11,12 +11,17 @@ const TodoList = () => {
     const { data: todos, error, loading } = useFetch(API_URL);
     const filteredTodos = useFilteredTodos(todos, searchTerm);
 
+    //OTTIMIZZA LA FUNZIONE DI RICERCA CON USECALLBACK
+    const handleSearchChange = useCallback((e) => {
+        setSearchTerm(e.target.value);
+    }, []);
+
     if (loading) return <p>Loading della pagina</p>
     if (error) return <p>Errore: {error}</p>
 
     return (
         <>
-            <input type="text" placeholder="Inserisci to-do da cercare" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+            <input type="text" placeholder="Inserisci to-do da cercare" value={searchTerm} onChange={handleSearchChange} />
             <ul>
                 {filteredTodos.map(todo => (
                     <li key={todo.id} >
