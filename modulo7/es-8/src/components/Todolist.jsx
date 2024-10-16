03bbf108b8635405f6a275c2703a77bde8acd212
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-16 22:47:28 +0100

    usato useMemo per memorizzare la lista filtrata di to-do

--- modulo7/es-8/src/components/Todolist.jsx
+++ modulo7/es-8/src/components/Todolist.jsx
@@ -1,6 +1,6 @@
 //UTILIZZA USEFETCH IN UN COMPONENTE
 
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { useFilteredTodos } from "../hooks/useFilteredTodos";
 
@@ -9,12 +9,18 @@ const API_URL = "https://jsonplaceholder.typicode.com/todos";
 const TodoList = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const { data: todos, error, loading } = useFetch(API_URL);
-    const filteredTodos = useFilteredTodos(todos, searchTerm);
 
     //OTTIMIZZA LA FUNZIONE DI RICERCA CON USECALLBACK
     const handleSearchChange = useCallback((e) => {
         setSearchTerm(e.target.value);
     }, []);
+
+    //MEMORIZZA LA LISTA DEI TO-DO FILTRATI CON USEMEMO
+    const filteredTodos = useMemo(() => {
+        if (!todos) return [];
+        return todos.filter(todo => todo.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        );
+    }, [todos, searchTerm]);
 
     if (loading) return <p>Loading della pagina</p>
     if (error) return <p>Errore: {error}</p>
