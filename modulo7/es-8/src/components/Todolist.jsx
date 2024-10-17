e59df7af71269dc9e38d5164e60b44e37e1dfa37
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-17 18:19:43 +0100

    gestito focus dell'input con useRef

--- modulo7/es-8/src/components/Todolist.jsx
+++ modulo7/es-8/src/components/Todolist.jsx
@@ -1,6 +1,6 @@
 //UTILIZZA USEFETCH IN UN COMPONENTE
 
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { useFilteredTodos } from "../hooks/useFilteredTodos";
 
@@ -9,6 +9,9 @@ const API_URL = "https://jsonplaceholder.typicode.com/todos";
 const TodoList = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const { data: todos, error, loading } = useFetch(API_URL);
+
+    //GESTISCI IL FOCUS DELL'INPUT DI RICERCA CON USEREF
+    const searchInputRef=useRef(null);
 
     //OTTIMIZZA LA FUNZIONE DI RICERCA CON USECALLBACK
     const handleSearchChange = useCallback((e) => {
@@ -22,12 +25,18 @@ const TodoList = () => {
         );
     }, [todos, searchTerm]);
 
+    useEffect(()=>{
+        if (searchInputRef.current) {
+            searchInputRef.current.focus();
+        }
+    }, []);
+
     if (loading) return <p>Loading della pagina</p>
     if (error) return <p>Errore: {error}</p>
 
     return (
         <>
-            <input type="text" placeholder="Inserisci to-do da cercare" value={searchTerm} onChange={handleSearchChange} />
+            <input type="text" placeholder="Inserisci to-do da cercare" value={searchTerm} onChange={handleSearchChange} ref={searchInputRef}/>
             <ul>
                 {filteredTodos.map(todo => (
                     <li key={todo.id} >
