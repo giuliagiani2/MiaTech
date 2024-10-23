b1dbbe7977c762ee49ce3ffdf67c3f43b6839576
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-23 12:51:50 +0100

    intregrato Layout in App

--- modulo7/es-9/src/App.jsx
+++ modulo7/es-9/src/App.jsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Layout from "./components/Layout";
 
 const App = () => {
 
   return (
     <>
-      <nav>
-        <Routes>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-        </Routes>
-      </nav>
+        </Route>
+      </Routes>
     </>
   )
 }
