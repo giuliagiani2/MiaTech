e0c6dc68c270435df78bf0d0a1f1dd90620eefa8
giuliagiani2 <giani.giulia2004@gmail.com>
2024-10-25 16:37:37 +0100

    usato useNavigate per andare alla home tramite un bottone

--- modulo7/es-9.1/src/main.jsx
+++ modulo7/es-9.1/src/main.jsx
@@ -7,5 +7,5 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
--- modulo7/es-9.1/src/pages/About.jsx
+++ modulo7/es-9.1/src/pages/About.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const About = () => {
+    const navigate = useNavigate();
+
+    const handleGoToHome = () => {
+        navigate("/");
+    }
 
     return (
         <>
             <h1>About</h1>
             <p>Qui ci sono le varie informazioni riguardanti l'esercizio fatto con vite in React.</p>
             <p>Installato React Router e create le pagine Home e About</p>
+            <button onClick={handleGoToHome}>Vai alla Home</button>
         </>
     );
 }
