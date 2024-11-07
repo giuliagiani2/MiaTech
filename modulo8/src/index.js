6726e31f934c1b35ad4fdd69df65d8fb5ed2cd0c
giuliagiani2 <giani.giulia2004@gmail.com>
2024-11-07 16:48:20 +0000

    utilizzare il Tipo Any

--- modulo8/src/index.js
+++ modulo8/src/index.js
@@ -1,73 +1,63 @@
-import { Todo } from "./types";
-
-const todos: Todo[] = [];
-let nextId: number = 1;
-
-const addTodo = (title: string, metadata?: any): Todo => {
-    const newTodo: Todo = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todos = [];
+let nextId = 1;
+const addTodo = (title, metadata) => {
+    const newTodo = {
         id: nextId,
         title: title,
         completed: false,
         metadata: metadata
-    }
+    };
     todos.push(newTodo);
     nextId++;
     return newTodo;
-}
-
+};
 //esempio
 addTodo("Ciao come stai", { priority: "high" });
 addTodo("Oggi è soleggiato");
-
 console.log(todos);
-
-
 //Associare Todo con Utenti
-const assignTodoToUser = (todoId: number, userId: number): Todo => {
+const assignTodoToUser = (todoId, userId) => {
     const todo = todos.find(t => t.id === todoId);
     if (todo) {
         todo.userId = userId;
         return todo;
     }
     return null;
-}
-
+};
 const assignedTodo = assignTodoToUser(1, 42);
 const assignedTodo1 = assignTodoToUser(2, 42);
 const assignedTodo2 = assignTodoToUser(3, 30);
-
 console.log(assignedTodo);
 console.log(assignedTodo1);
 console.log(assignedTodo2);
-
-
 //Creare una Funzione per Ottenere i Todo di un Utente
-const getUserTodos = (userId: number): Todo => {
+const getUserTodos = (userId) => {
     return todos.filter(todo => todo.userId === userId);
-}
+};
 const userAllTodos = getUserTodos(42);
-
-
 //Gestione degli Errori con Never
-const error = (message: string): never => {
+const error = (message) => {
     throw new Error(message);
-}
-
-
+};
 //Gestione dei Tipi Dinamici con Unknown
-const parseInput = (input: unknown): Todo => {
+const parseInput = (input) => {
     if (typeof input === "string") {
         return input;
-    } else if (typeof input === "number") {
+    }
+    else if (typeof input === "number") {
         return input.toString();
-    } else {
+    }
+    else {
         error("Input non valido");
     }
-}
+};
 parseInput("Hello world");
 parseInput(4);
 try {
     parseInput(true);
-} catch (error) {
+}
+catch (error) {
     console.error(error);
 }
--- modulo8/src/types.ts
+++ modulo8/src/types.ts
@@ -2,7 +2,8 @@ interface Todo {
     id: number,
     title: string,
     completed: boolean,
-    userId?: number
+    userId?: number,
+    metadata?: any
 }
 
 interface User {
