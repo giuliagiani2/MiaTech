ed99b1e6bacc3094d7809bb3af1f1ee1c1f9f661
giuliagiani2 <giani.giulia2004@gmail.com>
2024-11-07 15:23:35 +0000

    aggiungere una Funzione per Aggiungere un Todo

--- modulo8/src/index.ts
+++ modulo8/src/index.ts
@@ -1,3 +1,20 @@
-import { Todo } from types;
+import { Todo } from "./types";
 
 const todos: Todo[] = [];
+const nextId: number = 1;
+
+const addTodo = (title: string): Todo => {
+    const newTodo: Todo = {
+        id: nextId + 1,
+        title: title,
+        completed: false
+    }
+    todos.push(newTodo);
+    return newTodo;
+}
+
+//esempio
+addTodo("Ciao come stai");
+addTodo("Oggi è soleggiato");
+
+console.log(todos);
